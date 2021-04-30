c758cba25dae07825bdc1829427ba3f72365fdf9
Sumit Mandal <sumitmandal749@gmail.com>
2021-04-30 16:05:59 +0530

    Completed module weather app

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,24 +7,38 @@
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController, UITextFieldDelegate {
-
+class WeatherViewController: UIViewController {
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
     
     var weatherManager = WeatherManager()
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
+        weatherManager.delegate = self
         searchTextField.delegate = self
     }
+    
+    @IBAction func updateLocationButton(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+}
 
+//MARK: - UITextFieldDelegate
+extension WeatherViewController: UITextFieldDelegate {
+    
     @IBAction func searchPressed(_ sender: UIButton) {
-        print(searchTextField.text ?? "London")
         searchTextField.endEditing(true)
     }
     
@@ -52,6 +66,38 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
         
         searchTextField.text = ""
     }
+}
+
+//MARK:- WeatherManagerDelegate
+extension WeatherViewController: WeatherManagerDelegate {
+    
+    func didUpdateWeather(_ weatherManager: WeatherManager ,weather: WeatherModel) {
+        //An object that manages the execution of tasks serially or concurrently on your app's main thread or on a background thread.
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
     
 }
 
+//MARK: - CLLocationManagerDelegate
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -7,37 +7,83 @@
 //
 
 import Foundation
+import CoreLocation
+
+//Protocol
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager ,weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
 
 struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=0df6420f6825f494182e07a48fa9b38c&units=metric"
     
+    var delegate: WeatherManagerDelegate?
+    
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        performRequest(urlString: urlString)
+        performRequest(with: urlString)
     }
     
-    func performRequest(urlString: String) {
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
+        performRequest(with: urlString)
+    }
+    
+    //Perform Networking by gettin the required API URL
+    func performRequest(with urlString: String) {
         //1. Create URL
         if let url = URL(string: urlString) {
             //2. Create URL Session
             let session = URLSession(configuration: .default)
-            //3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            //3. Give the session a task            //Anonymous function closure
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let weather = parseJSON(safeData) {
+                        delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                }
+            }
             //4. Start the task
             task.resume()
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
+    //Parse the weather into an actual swift object
+    func parseJSON (_ weatherData: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
         
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString!)
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionID: id, cityName: name, temperature: temp)
+            return weather
+            
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
         }
-        
     }
 }
+
+
+//    func handle(data: Data?, response: URLResponse?, error: Error?) {
+//        if error != nil {
+//            print(error!)
+//            return
+//        }
+//
+//        if let safeData = data {
+//            let dataString = String(data: safeData, encoding: .utf8)
+//            print(dataString!)
+//        }
+//
+//    }
